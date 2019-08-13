28aa40717547b6fef58195e7f228d2b35b0139dd
Ebeo-Yvnjiang <54059423+Ebeo-Yvnjiang@users.noreply.github.com>
2019-08-13 16:36:48 +0800

    Create Ebeo.cpp

---
+++ Ebeo.cpp
@@ -0,0 +1,7 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+  cout<<"Hooray!Lo in China had made his first project in Fuzhou!!!\nHe loves ***.";
+  system("start Ebeo.exe");
+  return 0;
+}
