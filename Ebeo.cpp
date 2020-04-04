e513024d6cd99950ca2dbdc7ca8082555f7e182d
Billy2007 <54059423+Ebeo-Yvnjiang@users.noreply.github.com>
2020-04-04 18:01:04 +0800

    Delete Ebeo.cpp

--- Ebeo.cpp
+++
@@ -1,7 +0,0 @@
-#include<bits/stdc++.h>
-using namespace std;
-int main(){
-  cout<<"Hooray!Lo in China had made his first project in Fuzhou!!!\nHe loves ***.";
-  system("start Ebeo.exe");
-  return 0;
-}
